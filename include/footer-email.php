14cd62ce25d8620a573097f110de8209a1426e6d
Andryusha <egorov.2001@bk.ru>
2024-12-25 15:40:54 +0500

    Фикс контактов

---
+++ include/footer-email.php
@@ -0,0 +1,3 @@
+<div class="contacnt-footer-item">
+	<a href="mailto:klinikaeye@gmail.com" onclick="ym(87982252,'reachGoal','mail')"><span itemprop="email">klinikaeye@gmail.com</span></a>
+</div>
---
+++ include/footer-phone.php
@@ -0,0 +1,4 @@
+<div class="contacnt-footer-item">
+ <a href="tel:+73472265708" onclick="ym(87982252,'reachGoal','call')">
+     <i class="fa fa-phone icon-phone2 address-icons"></i> +7 (347) 226-57-08</a>
+</div>
